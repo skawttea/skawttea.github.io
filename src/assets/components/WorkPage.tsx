9a140d53105994ca6f087d1d60af3ce3ccb9bae4
Joshua R <111922365+Jrusli@users.noreply.github.com>
2025-09-10 00:18:20 -0700

    done

--- src/assets/components/WorkPage.tsx
+++ src/assets/components/WorkPage.tsx
@@ -23,31 +23,36 @@ function WorkPage() {
       </a>
       <a href="https://youtu.be/VK04pqnw0_c?si=9qHWw8hdvCd_3Vbh" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Bronco_Esports_Documentary_Ep._2_VALORANT.png" alt="Image 5" className="thumbnail-image" />
-        <p className="video-title">How a University Community Stays Relevant in one of the Biggest Esports Scenes</p>
+        <p className="video-title">How a University Stays Relevant in One of the Biggest Esports</p>
         <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
       </a>
+      <a href="https://youtu.be/VK04pqnw0_c?si=9qHWw8hdvCd_3Vbh" target="_blank" rel="noopener noreferrer">
+        <img src="Thumbnail\Usal_-_Central_Park_Community_Climb.png" alt="Image 6" className="thumbnail-image" />
+        <p className="video-title">Usal - Central Park Community Climb</p>
+        <p className="responsibility-text">Responsibilities: Editing</p>
+      </a>
       <a href="https://youtu.be/pJozfFshwpU?si=RTC-6d5ZygH6xG_j" target="_blank" rel="noopener noreferrer">
-        <img src="/Thumbnail/The_Poly_Post_-_Bronco_Esports_Documentary_Ep._3_Organization_President.png" alt="Image 6" className="thumbnail-image" />
+        <img src="/Thumbnail/The_Poly_Post_-_Bronco_Esports_Documentary_Ep._3_Organization_President.png" alt="Image 7" className="thumbnail-image" />
         <p className="video-title">How a College Club is Redefining Gaming Culture</p>
         <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
       </a>
       <a href="https://youtu.be/koCpaFBlKf0?si=M5qzRi0X37RDkkNp" target="_blank" rel="noopener noreferrer">
-        <img src="/Thumbnail/The_Poly_Post_-_Cal_State_GE_Change_News_Report.png" alt="Image 7" className="thumbnail-image" />
+        <img src="/Thumbnail/The_Poly_Post_-_Cal_State_GE_Change_News_Report.png" alt="Image 8" className="thumbnail-image" />
         <p className="video-title">Cal State General Education Coverage</p>
         <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
       </a>
       <a href="https://youtu.be/ce-StQdwCQs?si=KWiU1OYNORP1OEpt" target="_blank" rel="noopener noreferrer">
-        <img src="/Thumbnail/The_Poly_Post_-_Faculty_Union_Protest_Coverage.png" alt="Image 8" className="thumbnail-image" />
+        <img src="/Thumbnail/The_Poly_Post_-_Faculty_Union_Protest_Coverage.png" alt="Image 9" className="thumbnail-image" />
         <p className="video-title">California Faculty Association Protest Coverage</p>
         <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
       </a>
       <a href="https://youtu.be/BQvudSsh0Bo?si=lOPJcsAl_7ao73Lo" target="_blank" rel="noopener noreferrer">
-        <img src="/Thumbnail/The_Poly_Post_-_FragFest_2024_Event_Coverage.png" alt="Image 9" className="thumbnail-image" />
+        <img src="/Thumbnail/The_Poly_Post_-_FragFest_2024_Event_Coverage.png" alt="Image 10" className="thumbnail-image" />
         <p className="video-title">Frag Fest 2024 Event Coverage</p>
         <p className="responsibility-text">Responsibilities: Filming, Editing</p>
       </a>
       <a href="https://youtu.be/Lh8q97RrzGs?si=cDzQzALF038vYILx" target="_blank" rel="noopener noreferrer">
-        <img src="/Thumbnail/The_Poly_Post_-_Journalists_Try_Rock_Climbing.png" alt="Image 10" className="thumbnail-image" />
+        <img src="/Thumbnail/The_Poly_Post_-_Journalists_Try_Rock_Climbing.png" alt="Image 11" className="thumbnail-image" />
         <p className="video-title">Reporters Try Rock Climbing for the First Time</p>
         <p className="responsibility-text">Responsibilities: Editing, On-Camera Personality</p>
       </a>
