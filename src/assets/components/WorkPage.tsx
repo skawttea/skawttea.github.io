50c2f0b81066006cb20b1d403fc3db0f880bdf8f
Joshua R <111922365+Jrusli@users.noreply.github.com>
2025-10-31 00:15:16 -0700

    done

--- src/assets/components/WorkPage.tsx
+++ src/assets/components/WorkPage.tsx
@@ -4,7 +4,7 @@ function WorkPage() {
       <a href="https://www.youtube.com/watch?v=nDMLeo0Qz-w" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/Personal_YouTube_Channel_-_Emotions_in_Video_Games.jpg" alt="Image 1" className="thumbnail-image" />
         <p className="video-title">emotions in video games</p>
-        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality, Color Grading</p>
       </a>
        <a href="https://www.instagram.com/reel/DQXDQ6-j7JA/" target="_blank" rel="noopener noreferrer">
       <img src="/Thumbnail/dfgjdgjfdt6yik.png" alt="Image 2" className="thumbnail-image" />
@@ -14,17 +14,17 @@ function WorkPage() {
       <a href="https://youtu.be/ZnWIXqwIt0U?si=StqQJvsA7cwnfY0n" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Bronco_Esports_Documentary_Ep._1_Melee.png" alt="Image 3" className="thumbnail-image" />
         <p className="video-title">How College Students Help Keep a 20 Year Old Game Alive</p>
-        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality, Color Grading</p>
       </a>
       <a href="https://youtu.be/v6OI8cK4QVU?si=xf3Ritq0DLZnR2UA" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Bronco_Fusion_2024_Concert_Coverage_Ft._J.I.D..png" alt="Image 4" className="thumbnail-image" />
         <p className="video-title">J.I.D. Headlines Bronco Fusion 2024</p>
-        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality, Color Grading</p>
       </a>
       <a href="https://www.instagram.com/reel/DNoaPaOyYSt/?igsh=MTg1endrd2tibXh6dw==" target="_blank" rel="noopener noreferrer">
       <img src="/Thumbnail/Usal_x_Gorp_Girls_-_Hiking_Reel_v2.png" alt="Image 2" className="thumbnail-image" />
         <p className="video-title">Usal X Gorp Girls</p>
-        <p className="responsibility-text">Responsibilities: Editing</p>
+        <p className="responsibility-text">Responsibilities: Editing, Color Grading</p>
       </a>
       <a href="https://youtu.be/AeDc2BMAiuQ?si=NUSQ4A17GuNgFMiW" target="_blank" rel="noopener noreferrer">
       <img src="/Thumbnail/Newsroom_Sessions_Raccoon_Cartoons.png" alt="Image 2" className="thumbnail-image" />
@@ -34,37 +34,37 @@ function WorkPage() {
       <a href="https://youtu.be/VK04pqnw0_c?si=9qHWw8hdvCd_3Vbh" target="_blank" rel="noopener noreferrer">
         <img src="Thumbnail\Usal_-_Central_Park_Community_Climb.png" alt="Image 5" className="thumbnail-image" />
         <p className="video-title">Usal - Central Park Community Climb</p>
-        <p className="responsibility-text">Responsibilities: Editing</p>
+        <p className="responsibility-text">Responsibilities: Editing, Color Grading</p>
       </a>
       <a href="https://youtu.be/VK04pqnw0_c?si=9qHWw8hdvCd_3Vbh" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Bronco_Esports_Documentary_Ep._2_VALORANT.png" alt="Image 6" className="thumbnail-image" />
         <p className="video-title">How a University Stays Relevant in One of the Biggest Esports</p>
-        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality, Color Grading</p>
       </a>
       <a href="https://youtu.be/pJozfFshwpU?si=RTC-6d5ZygH6xG_j" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Bronco_Esports_Documentary_Ep._3_Organization_President.png" alt="Image 7" className="thumbnail-image" />
         <p className="video-title">How a College Club is Redefining Gaming Culture</p>
-        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality, Color Grading</p>
       </a>
       <a href="https://youtu.be/koCpaFBlKf0?si=M5qzRi0X37RDkkNp" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Cal_State_GE_Change_News_Report.png" alt="Image 8" className="thumbnail-image" />
         <p className="video-title">Cal State General Education Coverage</p>
-        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality, Color Grading</p>
       </a>
       <a href="https://youtu.be/ce-StQdwCQs?si=KWiU1OYNORP1OEpt" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Faculty_Union_Protest_Coverage.png" alt="Image 9" className="thumbnail-image" />
         <p className="video-title">California Faculty Association Protest Coverage</p>
-        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Writing, Filming, Editing, On-Camera Personality, Color Grading</p>
       </a>
       <a href="https://youtu.be/BQvudSsh0Bo?si=lOPJcsAl_7ao73Lo" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_FragFest_2024_Event_Coverage.png" alt="Image 10" className="thumbnail-image" />
         <p className="video-title">Frag Fest 2024 Event Coverage</p>
-        <p className="responsibility-text">Responsibilities: Filming, Editing</p>
+        <p className="responsibility-text">Responsibilities: Filming, Editing, Color Grading</p>
       </a>
       <a href="https://youtu.be/Lh8q97RrzGs?si=cDzQzALF038vYILx" target="_blank" rel="noopener noreferrer">
         <img src="/Thumbnail/The_Poly_Post_-_Journalists_Try_Rock_Climbing.png" alt="Image 11" className="thumbnail-image" />
         <p className="video-title">Reporters Try Rock Climbing for the First Time</p>
-        <p className="responsibility-text">Responsibilities: Editing, On-Camera Personality</p>
+        <p className="responsibility-text">Responsibilities: Editing, On-Camera Personality, Color Grading</p>
       </a>
     </div>
   );
